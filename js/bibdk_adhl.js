8bbb265f0eadd1787ad2b97ee3996741056ea1c8
Søren Vibjerg <vibjerg@gmail.com>
2013-02-04 15:52:00 +0100

    Ajax implementation of adhl request.

---
+++ js/bibdk_adhl.js
@@ -0,0 +1,44 @@
+(function ($) {
+
+    /** Insert adhl results */
+    Drupal.addRecommendation = function (adhl) {
+        console.log(adhl);
+        if(adhl.error)
+            $('.recommendation-load[data-pid=' + adhl.pid + ']').replaceWith(adhl.error);
+        if (adhl.list)
+            $('.recommendation-load[data-pid=' + adhl.pid + ']').replaceWith(adhl.list);
+        if (adhl.more)
+            $('.recommendation-load[data-pid=' + adhl.pid + ']').append(adhl.more);
+    },
+        Drupal.loadRecommendation = function(element){
+            var pid = $(element).attr('data-pid');
+            /* Add throbber*/
+            $(element).addClass('ajax-progress');
+            $(element).html('<span class="throbber">&nbsp;</span>');
+
+            /* Call ajax */
+            var request = $.ajax({
+                url:Drupal.settings.basePath + 'adhl/ajax',
+                type:'POST',
+                data:{
+                    pid:pid
+                },
+                dataType:'json',
+                success:Drupal.addRecommendation
+            });
+        }
+    /** Get holdingstatus via ajax */
+    Drupal.behaviors.recommendation = {
+
+        attach:function (context) {
+            $('.recommendation-load', context).each(function (i, element) {
+                Drupal.loadRecommendation(element);
+            });
+        },
+        detach:function (context) {
+
+        }
+    };
+
+}(jQuery));
+
