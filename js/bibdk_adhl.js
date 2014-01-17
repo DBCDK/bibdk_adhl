f2c90cc1108acd15dfd13a56454a816662322dc1
JrgenGNielsen <jgn@dbc.dk>
2014-01-17 15:59:22 +0100

    replace toggle_text if errors

--- js/bibdk_adhl.js
+++ js/bibdk_adhl.js
@@ -2,36 +2,34 @@
 
     /** Insert adhl results */
     Drupal.addRecommendation = function (adhl) {
-        if (adhl.error) {
-            if ( adhl.error == 'bibdk_adhl_no_recommendations' ) {
-                // $('.recommendation-load[data-pid=' + adhl.pid + ']').parent().parent().addClass('visuallyhidden');
-                $('.recommendation-load[data-pid=' + adhl.pid + ']').parent().parent().find('.toggle-text').html(adhl.toggle_text);;
-            }
-            $('.recommendation-load[data-pid=' + adhl.pid + ']').replaceWith(adhl.error);
+        if ( adhl.error ) {
+            $('.recommendation-load[data-pid=' + adhl.pid + ']').parent().parent().find('.toggle-text').html(adhl.toggle_text);
+            $('.recommendation-load[data-pid=' + adhl.pid + ']').replaceWith(adhl.error_msg);
         }
-        if (adhl.list) {
+        if ( adhl.list ) {
             $('.recommendation-load[data-pid=' + adhl.pid + ']').replaceWith(adhl.list);
             $('.recommendation-more[data-pid=' + adhl.pid + ']').removeClass('visuallyhidden');
         }
     },
-        Drupal.loadRecommendation = function (element) {
-            var pid = $(element).attr('data-pid');
-            var isbn = $(element).attr('data-isbn');
-            /* Add throbber*/
-            $(element).addClass('ajax-progress');
-            $(element).html('<span class="throbber">&nbsp;</span>');
+    Drupal.loadRecommendation = function (element) {
+        var pid = $(element).attr('data-pid');
+        var isbn = $(element).attr('data-isbn');
+        /* Add throbber*/
+        $(element).addClass('ajax-progress');
+        $(element).html('<span class="throbber">&nbsp;</span>');
 
-            /* Call ajax */
-            var request = $.ajax({
-                url:Drupal.settings.basePath + 'adhl/ajax',
-                type:'POST',
-                data:{
-                    pid:pid
-                },
-                dataType:'json',
-                success:Drupal.addRecommendation
-            });
-        };
+        /* Call ajax */
+        var request = $.ajax({
+            url:Drupal.settings.basePath + 'adhl/ajax',
+            type:'POST',
+            data:{
+                pid:pid
+            },
+            dataType:'json',
+            success:Drupal.addRecommendation
+        });
+    };
+
     /** Get holdingstatus via ajax */
     Drupal.behaviors.recommendation = {
 
@@ -44,5 +42,6 @@
 
         }
     };
+
 }(jQuery));
 
