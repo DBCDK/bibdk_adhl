768060baf590e673afb080ef043a5ae96a8a5b10
Søren Vibjerg <vibjerg@gmail.com>
2014-04-04 10:45:05 +0200

    add template for top list

---
+++ theme/bibdk-adhl-toplist.tpl.php
@@ -0,0 +1,5 @@
+<p class="adhl-toplist-before-text"><?php print $before_text; ?></p>
+<div class="adhl-toplist">
+  <?php print drupal_render($list); ?>
+</div>
+<p class="adhl-toplist-after-text"><?php print $after_text; ?></p>
